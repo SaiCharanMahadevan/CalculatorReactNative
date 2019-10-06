ad2ef403308cf53cc011d839881ed252cda74948
Sai Charan Mahadevan <saicharan712@gmail.com>
2019-10-07 04:02:18 +0800

    MISC: Update sample image for calculator

--- constants/misc.js
+++ constants/misc.js
@@ -9,7 +9,7 @@ export const BUTTONS_NUMBERS = [
   ['4', '5', '6'],
   ['1', '2', '3']
 ];
-export const CLEAR = 'DELETE';
+export const CLEAR = 'CLEAR';
 export const ZERO = '0';
 export const EQUALS = '=';
 export const ERROR_MESSAGE = `Can't do this!`;
